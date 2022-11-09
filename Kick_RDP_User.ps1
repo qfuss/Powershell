b741ab9b07d1b275c83570eb64b764b837e32dca
qfuss <romb@mailbox.org>
2022-11-09 09:27:45 +0100

    Create Kick_RDP_User.ps1
    Remove all active RDP-Sessions

---
+++ Kick_RDP_User.ps1
@@ -0,0 +1,5 @@
+$ids= Get-RDUserSession -ConnectionBroker "FQDN" | where-Object {$_.UserName -notlike "administrator" } #| select-object UnifiedSessionID 
+
+foreach ($id in $ids){
+    Logoff $id.UnifiedSessionId /server:Hostname
+} 
